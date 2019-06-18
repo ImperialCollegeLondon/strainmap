20bcd6ef71b7785ebfb6f005fb1bbc3a7ff916bf
Diego <d.alonso-alvarez@imperial.ac.uk>
2019-06-18 15:19:38 +0100

    :white_check_mark: Add tests for base_classes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,7 @@ def dicom_bg_data_path():
 
 
 @fixture
-def mock_registered_views():
+def registered_views():
     from strainmap.gui.data_view import DataView
     from strainmap.gui.segmentation_view import SegmentationView
 
@@ -26,8 +26,32 @@ def mock_registered_views():
 
 @fixture
 @patch("strainmap.gui.base_classes.MainWindow", autospec=True)
-def control_with_mock_window(MockWindow, mock_registered_views):
+def control_with_mock_window(MockWindow, registered_views):
     from strainmap.main import StrainMap
 
-    StrainMap.registered_views = mock_registered_views
+    StrainMap.registered_views = registered_views
     return StrainMap()
+
+
+@fixture
+def main_window():
+    from strainmap.gui.base_classes import MainWindow
+
+    return MainWindow()
+
+
+@fixture
+def empty_view():
+    from strainmap.gui.base_classes import ViewBase
+
+    class TestView(ViewBase):
+        def __init__(self, root, actions):
+            super().__init__(root, actions)
+
+        def update_widgets(self):
+            pass
+
+        def clear_widgets(self):
+            pass
+
+    return TestView
