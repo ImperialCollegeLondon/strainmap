bcab0f4705ae29c6c160b49053e04abaf2430276
Diego <d.alonso-alvarez@imperial.ac.uk>
2019-06-19 11:13:14 +0100

    :white_check_mark: Add tests for the data view.

--- tests/test_data_view.py
+++ tests/test_data_view.py
@@ -3,11 +3,20 @@ from pytest import mark
 
 def test_load_data_button(data_view, dicom_data_path):
 
-    data_view.control.children["newSeries"].children["chooseDataFolder"].invoke()
+    data_view.nametowidget("control.chooseDataFolder").invoke()
+
     assert data_view.actions.load_data.call_count == 1
     assert data_view.data_folder.get() == str(dicom_data_path)
 
     data_view.actions.load_data.reset_mock()
+
+
+def test_clear_data_button(data_view):
+
+    data_view.nametowidget("control.clearAllData").invoke()
+    assert data_view.actions.clear_data.call_count == 1
+
+    data_view.actions.clear_data.reset_mock()
 
 
 @mark.skip("Functionality ot implemented, yet.")
@@ -20,9 +29,18 @@ def test_output_button(data_view):
     pass
 
 
-def test_clear_data_button(data_view):
+def test_update_and_clear_widgets(data_view, strainmap_data):
 
-    data_view.control.children["clearAllData"].invoke()
-    assert data_view.actions.clear_data.call_count == 1
+    data_view.data = strainmap_data
 
-    data_view.actions.clear_data.reset_mock()
+    assert data_view.nametowidget("visualise.notebook")
+    assert len(data_view.notebook.tabs()) == 2
+    assert data_view.nametowidget("control.chooseOutputFile")["state"] == "enable"
+
+    data_view.data = None
+
+    assert not data_view.notebook
+    assert (
+        str(data_view.nametowidget("control.chooseOutputFile")["state"]) == "disabled"
+    )
+    assert data_view.data_folder.get() == ""
