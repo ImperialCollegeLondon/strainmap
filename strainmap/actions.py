a506614a8ff7cc06ff2432077f8cc01238fdb6f8
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2019-06-25 09:19:40 +0100

    Remove unnecessary wrapper in decorator

--- strainmap/actions.py
+++ strainmap/actions.py
@@ -1,36 +1,30 @@
-""" This module contains the functions that interface together the GUI, the model and
-the main control object.
+"""This module contains the functions that interface together the GUI, the
+model and the main control object.
 
 All this functions take as first argument a StrainMap object, which will update the
 data available in the views, and none or more keyword arguments that are the actual
 inputs to the model functions.
 """
-from functools import wraps
 from typing import Callable
 
 from .gui.base_classes import Requisites
 from .model.strainmap_data_model import factory
-
 
 REGISTERED_ACTIONS = {}
 """ Registered actions."""
 
 
 def register_action(fun: Callable):
-    """Registers an action that can be executed by the views. """
-
-    @wraps(fun)
-    def wrapper(*args, **kwargs):
-        return fun(*args, **kwargs)
+    """Registers an action that can be executed by the views."""
 
     REGISTERED_ACTIONS[fun.__name__] = fun
 
-    return wrapper
+    return fun
 
 
 @register_action
 def load_data(control, **kwargs):
-    """ Creates a StrainMapData object. """
+    """Creates a StrainMapData object."""
     data = factory(**kwargs)
     control.unlock(Requisites.DATALOADED)
     control.update_views(data)
@@ -38,6 +32,6 @@ def load_data(control, **kwargs):
 
 @register_action
 def clear_data(control, **kwargs):
-    """ Clears the StrainMapData object from the widgets. """
+    """Clears the StrainMapData object from the widgets."""
     control.lock(Requisites.DATALOADED)
     control.update_views(None)
