b763ac3d94d1fbbbdc77999ab0c76d6eb557427f
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2019-07-08 12:04:43 +0100

    More specific control of what is patched when

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,25 +3,37 @@ from unittest.mock import patch
 
 from pytest import fixture
 
-patch("tkinter.filedialog.askdirectory", lambda *x, **y: _dicom_data_path()).start()
-patch("tkinter.messagebox.askokcancel", lambda *x, **y: "ok").start()
-patch("tkinter.messagebox.showinfo", lambda *x, **y: "ok").start()
+
+def patch_dialogs(function):
+    from functools import wraps
+
+    @wraps(function)
+    def decorated(*args, **kwargs):
+
+        with patch(
+            "tkinter.filedialog.askdirectory", lambda *x, **y: _dicom_data_path()
+        ):
+            with patch("tkinter.messagebox.askokcancel", lambda *x, **y: "ok"):
+                with patch("tkinter.messagebox.showinfo", lambda *x, **y: "ok"):
+                    return function(*args, **kwargs)
+
+    return decorated
 
 
 def _dicom_data_path():
-    """ Returns the DICOM data path. """
+    """Returns the DICOM data path."""
     return Path(__file__).parent / "data" / "SUB1"
 
 
 @fixture(scope="session")
 def dicom_data_path():
-    """ Returns the DICOM data path. """
+    """Returns the DICOM data path."""
     return _dicom_data_path()
 
 
 @fixture(scope="session")
 def data_tree(dicom_data_path):
-    """ Returns the DICOM directory data tree. """
+    """Returns the DICOM directory data tree."""
     from strainmap.models.readers import read_dicom_directory_tree
 
     return read_dicom_directory_tree(dicom_data_path)
@@ -29,7 +41,7 @@ def data_tree(dicom_data_path):
 
 @fixture(scope="session")
 def strainmap_data(dicom_data_path):
-    """ Returns a loaded StrainMapData object. """
+    """Returns a loaded StrainMapData object."""
     from strainmap.models.strainmap_data_model import factory
 
     return factory(data_files=dicom_data_path)
@@ -37,7 +49,7 @@ def strainmap_data(dicom_data_path):
 
 @fixture(scope="session")
 def dicom_bg_data_path():
-    """ Returns the DICOM background data path. """
+    """Returns the DICOM background data path."""
     return Path(__file__).parent / "data" / "SUB1_BG"
 
 
@@ -50,12 +62,12 @@ def registered_views():
 
 
 @fixture
-@patch("strainmap.gui.base_window_and_task.MainWindow", autospec=True)
-def control_with_mock_window(MockWindow, registered_views):
+def control_with_mock_window(registered_views):
     from strainmap.controller import StrainMap
 
     StrainMap.registered_views = registered_views
-    return StrainMap()
+    with patch("strainmap.gui.base_window_and_task.MainWindow", autospec=True):
+        return StrainMap()
 
 
 @fixture(scope="session")
