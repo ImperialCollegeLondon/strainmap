96bd446e7a3015db4524699df67c96fa37f15f9b
Diego <d.alonso-alvarez@imperial.ac.uk>
2019-08-07 09:45:51 +0100

    Add documentation to the segmentation script.

--- examples/segmentation_script.py
+++ examples/segmentation_script.py
@@ -1,3 +1,27 @@
+"""This script contains a simple example on how to run a segmentation of one or more
+images providing the initial contour and setting the segmentation model, filter,
+propagator and their corresponding input parameters.
+
+Installation instructions on how to have StrainMap up and running are included in the
+README.md file of the StrainMap repository.
+
+To run this script simply activate the virtual environment created in the installation
+instructions and then run:
+
+$ python path_to_this_script/segmentation_script.py
+
+Full details of the inputs of the 'simple_segmentation' function can be obtained with:
+
+>>> from strainmap.models.quick_segmentation import simple_segmentation
+>>> help(simple_segmentation)
+
+The available parameters for the model and filter functions - as well as their default
+values - can be found in:
+
+https://scikit-image.org/docs/dev/api/skimage.segmentation.html
+https://scikit-image.org/docs/dev/api/skimage.filters.html
+"""
+
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -11,7 +35,7 @@ path = Path(__file__).parent.parent / "tests/data/SUB1/MR024001000.dcm"
 image = pydicom.dcmread(str(path)).pixel_array
 
 # Create the initial contour
-init = Contour.circle((310, 280), radius=60, shape=image.shape).xy
+init = Contour.circle(center=(310, 280), radius=60, shape=image.shape).xy
 
 # Choose the models and parameters. See 'simple_segmentation' for a description of
 # all the options
@@ -36,7 +60,7 @@ segment = simple_segmentation(
 
 # Plot the results
 plt.imshow(image)
-plt.plot(init[:, 0], init[:, 1], "r", label="Initial")
-plt.plot(segment[:, 0], segment[:, 1], "y", label="Segmented")
+plt.plot(*init.T, "r", label="Initial")
+plt.plot(*segment.T, "y", label="Segmented")
 plt.legend()
 plt.show()
