:bug: Invert origin coordinates in the radial segments calculation.

strainmap/models/contour_mask.py
@@ -247,16 +247,16 @@ def radial_segments(
          [0 0 0 0 0 0 0 3 0 0 0 0 0 0 0]
          [0 0 0 0 3 2 2 2 3 3 3 0 0 0 0]
          [0 0 0 3 1 1 1 1 2 2 3 3 0 0 0]
-         [0 0 3 1 0 0 0 0 1 2 2 3 3 0 0]
+         [0 0 3 1 0 0 0 0 1 1 2 3 3 0 0]
          [0 0 2 0 0 0 0 0 0 1 2 2 3 0 0]
          [0 0 2 0 0 0 0 0 0 1 2 2 3 0 0]
          [0 0 2 0 0 0 0 0 0 1 2 2 3 3 0]
          [0 0 2 1 0 0 0 0 1 1 2 2 3 0 0]
          [0 0 3 2 1 1 1 1 1 2 2 3 3 0 0]
-         [0 0 3 2 2 2 1 1 2 2 2 3 3 0 0]
+         [0 0 3 3 2 2 1 1 2 2 2 3 3 0 0]
          [0 0 0 3 3 2 2 2 2 3 3 3 0 0 0]
          [0 0 0 0 3 3 3 3 3 3 3 0 0 0 0]
-         [0 0 0 0 0 0 0 3 0 0 0 0 0 0 0]
+         [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
          [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]]
 
         Note that it is an error if the outer and inner boundaries are swapped:
@@ -280,9 +280,9 @@ def radial_segments(
 
     thetas = np.arctan2(y, x)
     r = np.sqrt(x * x + y * y)
-    polar = cart2pol(outer.xy - origin)
+    polar = cart2pol(outer.xy - origin[::-1])
     outer_pol = np.interp(thetas, polar.theta, polar.r, period=2 * np.pi)
-    polar = cart2pol(inner.xy - origin)
+    polar = cart2pol(inner.xy - origin[::-1])
     inner_pol = np.interp(thetas, polar.theta, polar.r, period=2 * np.pi)
 
     # ensure numerical noise doesn't push the inner contour to the outside

strainmap/models/velocities.py
@@ -58,21 +58,20 @@ def scale_phase(
     if swap:
         phase[0], phase[1] = phase[1], phase[0].copy()
 
-    phase[0] *= (-1) ** sign_reversal[0]
-    phase[1] *= (-1) ** sign_reversal[1]
-    phase[2] *= (-1) ** sign_reversal[2]
+    for i, r in enumerate(sign_reversal):
+        phase[i] *= (-1) ** r
 
     return phase
 
 
 def global_masks_and_origin(outer, inner, img_shape):
     """Finds the global masks and origin versus time frame."""
-    masks = [
-        contour_diff(outer[i].T, inner[i].T, img_shape).T for i in range(len(outer))
-    ]
-    origin = list(map(ndimage.measurements.center_of_mass, masks))
+    masks = np.array(
+        [contour_diff(o.T, i.T, img_shape).T for o, i in zip(outer, inner)]
+    )
+    origin = np.array([*map(ndimage.measurements.center_of_mass, masks)])
 
-    return np.array(masks), np.array(origin)
+    return masks, origin
 
 
 def transform_to_cylindrical(phase: np.ndarray, masks: np.ndarray, origin: np.ndarray):
@@ -113,7 +112,7 @@ def velocity_global(cylindrical: np.ndarray, mask: np.ndarray, bg: str):
     return velocities, masks
 
 
-def velocities_angular_segments(
+def velocities_angular(
     cylindrical: np.ndarray,
     zero_angle: np.ndarray,
     origin: np.ndarray,
@@ -138,7 +137,7 @@ def velocities_angular_segments(
     return velocities, masks
 
 
-def velocities_radial_segments(
+def velocities_radial(
     cylindrical: np.ndarray,
     segments: Dict[str, np.ndarray],
     origin: np.ndarray,
@@ -209,7 +208,7 @@ def calculate_velocities(
         vel_labels += list(velocities.keys())
 
     if angular_regions:
-        velocities, masks = velocities_angular_segments(
+        velocities, masks = velocities_angular(
             cylindrical,
             data.zero_angle[dataset_name],
             origin,
@@ -222,7 +221,7 @@ def calculate_velocities(
         vel_labels += list(velocities.keys())
 
     if radial_regions:
-        velocities, masks = velocities_radial_segments(
+        velocities, masks = velocities_radial(
             cylindrical, data.segments[dataset_name], origin, bg, radial_regions
         )
         data.velocities[dataset_name].update(velocities)