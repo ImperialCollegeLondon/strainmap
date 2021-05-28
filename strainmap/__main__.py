453197310484734dbc56d51081772fe01ee11013
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-05-28 15:46:16 +0100

    Set logging level

--- strainmap/__main__.py
+++ strainmap/__main__.py
@@ -1,4 +1,9 @@
+import logging
+
+import decouple
+
 from strainmap import controller
 
+logging.basicConfig(level=decouple.config("STRAINMAP_LOGGING_LEVEL", "WARNING"))
 if __name__ == "__main__":
     controller.StrainMap().run()
