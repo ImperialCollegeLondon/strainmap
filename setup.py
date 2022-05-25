9f015a8433a9a3c6e44914fd36371901e2e027a9
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-05-25 13:43:47 +0100

    Bump version: 1.0.5 → 1.0.6

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ pyinstaller = ["pyinstaller"]
 
 setup(
     name="StrainMap",
-    version="1.0.5",
+    version="1.0.6",
     url="https://github.com/imperialcollegelondon/strainmap",
     author="Research Computing Service, Imperial College London",
     author_email="rcs-support@imperial.ac.uk",
